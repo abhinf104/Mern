3d6cc0fc0ab25f2d0fd44e8e9597501b0dd73486
Abhishek Kumar <abhinf104@gmail.com>
2024-10-23 02:06:05 +0530

    Project1(P1-TaskManager Added)

--- Project/P1_TaskManager/server/routes/tasks.js
+++ Project/P1_TaskManager/server/routes/tasks.js
@@ -1,5 +1,10 @@
+// Import the express module
 const express = require("express");
+
+// Create a new router object
 const router = express.Router();
+
+// Destructure the task controller functions from the tasks module
 const {
   getTasks,
   postTasks,
@@ -8,8 +13,11 @@ const {
   deleteId,
 } = require("../controller/tasks");
 
+// Define the route for getting and posting tasks
 router.route("/").get(getTasks).post(postTasks);
 
+// Define the route for getting, updating, and deleting a task by ID
 router.route("/:id").get(getId).patch(patchId).delete(deleteId);
 
+// Export the router object to be used in other parts of the application
 module.exports = router;
